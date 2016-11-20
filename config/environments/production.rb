3700229dab25e946115dc4b76c979438ec7d6759
triniz <triki.nizar@gmail.com>
2016-11-20 14:18:21 -0500

    sendgrid - prod v1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
     
   config.action_mailer.default_url_options ={ host: 'https://shielded-garden-78290.herokuapp.com'}
-  config.action_mailer.delivery_metho = :smtp
+  config.action_mailer.delivery_method = :smtp
     
   ActionMailer::Base.smtp_settings = {
       :address        => 'smtp.sendgrid.net',
